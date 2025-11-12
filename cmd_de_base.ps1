8aef698a323d5ac663aedf1339abea11c436d461
Ressil9 <essilor.itq@gmail.com>
2025-11-12 15:57:19 +0100

    start powershell

---
+++ cmd_de_base.ps1
@@ -0,0 +1,6 @@
+Get-Service
+Get-Date
+#permet de trouver les commandes
+Get-Command -noun Service
+#permet de donner des infos sur la commande 
+Get-Help Get-Service -Full
---
+++ variable.ps1
@@ -0,0 +1,14 @@
+$myString = "Ben prog"
+$myNumber = 5
+#on cast en double 
+[double]$myNumber2 = 9
+
+$mySum = $myNumber + $myNumber2
+
+$myBool = $true
+
+($myNumber -eq $myNumber2)
+
+$myNumber.GetType()
+
+$mySum
