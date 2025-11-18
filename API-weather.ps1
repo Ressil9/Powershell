89aa70e1fed69ff9ca3bcb958b9c6f571f7ad318
Ressil9 <essilor.itq@gmail.com>
2025-11-18 14:31:53 +0100

    Notions PW

---
+++ API-weather.ps1
@@ -0,0 +1,12 @@
+﻿$uri = " https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&current_weather=true"
+
+
+$response = Invoke-RestMethod -Uri $uri -Method Get
+
+$response
+
+$CW = $response.current_weather
+
+$CW_U = $response.current_weather_units
+
+
